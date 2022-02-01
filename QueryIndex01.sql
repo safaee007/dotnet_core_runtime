4e412f8cc8f27eabe93aa1d2c314c228259e9cdc
Saeed Safaee <saeedsafaee144@gmail.com>
2022-02-01 10:30:43 +0330

    Add files via upload

---
+++ QueryIndex01.sql
@@ -0,0 +1,86 @@
+﻿--index0
+--select * from sys.sysfiles
+
+--show page in table
+--DBCC IND('QadatonaV4', 'qad_Articles', 1)
+
+--DBCC traceon(3604)
+--DBCC page('QadatonaV4', 'qad_Articles', 1, 4723, 3)
+
+--log file
+--DBCC log('QadatonaV4')
+
+----------------------Part2------------------------
+use Northwind
+go
+
+set statistics io on	
+go
+------------------------------------
+--select * from orders --its very important(bad) logical reads 22, physical reads 1,
+-----logical reads 22 page -- پیح های خوانده شده
+-----read-ahead reads -- خواندی پیش پیش که دیتاهایی که آینده ممکنه که شما درخواست بدی
+
+----DBCC DROPCLEANBUFFERS --- پاک کردن فضای بافر شده که دوباره باید (آی او) بزند بررسی تعداد درخواست های فیزیکی
+--select OrderID,OrderDate from Orders -- its very important logical reads 4, physical reads 1
+--------------------------------------
+--set statistics time On
+--select OrderID, OrderDate from orders
+--DBCC DROPCLEANBUFFERS
+-------------------------------------
+------Execution Plan بررسی 
+--select * from Orders
+--inner join [Order Details] on Orders.OrderID = [Order Details].OrderID
+
+----------------------Part3------------------------
+--جستجو 3 حالت در جدول
+--جدول مرتب ن شده 
+--چدول مرتب شده
+--جدول دارای اندیکس که دیتاهای هر جدول در ایندکس مربوطه است و برای جستجو در ایندکس رفته و زمان صرفه جوی میشود
+
+----------------------Part4------------------------
+--لیستی از داده ها که بر اساس موضوع خاص مرتب شده باشد(الفبا، عدد یا ...) را ایندکس میگویند
+--دلایل ایندکس:
+--سرعت بازیابی رکورد
+-- افزایش سرعت عملیات جوین یا پیوند
+--اعمال یکتای روی داده
+--انواع ایندکس:
+--1 Cluster Index -- all data
+--2 None CLuster Index -- all data
+--3 XML Index -- -- xml data
+--4 Special Index -- jeoGeraphy data
+--5 Column Store Index -- very Good if use this index table is readonly
+
+----------------------Part5------------------------
+--Select * into Orders2 From Orders --make table in query
+--Select Count(OrderID) From Orders
+--Select Count(OrderID) From Orders2
+
+-----count index
+--SP_HELPIndex Orders
+
+--select * from Orders
+--select * from Orders2
+
+--select * from Orders where	OrderID= 106
+--select * from Orders2 where	OrderID= 106
+----------------------Part6------------------------
+--B+Tree OR Balanced Tree for IndexStructure
+--RootLevel==> MidelLevel==> LeafLevel
+--startRoot Tree for search: sysundex
+----------------------Part7------------------------
+--CulsterIndex:
+--select * into Customers2 from Customers
+--select * from Customers
+--select * from Customers2
+--SP_HELPINDEX Customers2
+--create clustered index ix_customer2 on Customers2(CustomerID)
+--select * from customers2
+
+
+
+
+
+
+
+
---
+++ QueryIndex02.sql
@@ -0,0 +1,63 @@
+﻿
+use Northwind
+go
+--create table Students
+--(
+-- Code int not null,
+-- Firsname nvarchar(50),
+-- lastname nvarchar(50)
+--)
+--SP_HELPINDEX Students
+
+--insert into Students values
+--(100, N'سعید', 'صفایی'),
+--(101, N'علی', 'تست زاده'),
+--(102, N'تست', 'پروان'),
+--(103, N'فرید', 'اگبری'),
+--(104, N'مسعود', '5صفایی')
+
+--select * from Students
+--truncate table students
+--create Clustered Index IX_Students_Code On Students(Code)
+--insert into Students values(103, N'یاور', N'علیپور')
+
+
+--DECLARE @counter int=1;
+--while @counter <= 1000
+--BEGIN
+--	insert into Students values(@counter, N'firstname'+CAST(@counter as nvarchar(10)), 'lastname'+CAST(@counter as nvarchar(10)))
+--	set @counter = @counter + 1
+--END
+--GO
+
+--create Unique Clustered Index IX_Students_Code On Students(Code) ایندکس یکتا
+--در صورتی که یکتا بودن مهم است انرا اجرا کنید که در زمان  افزودن رکورد جدید یک وقفه برای چک کردن اینکه این داده از قبل نباشد دارد
+-------------------------Part2-------------------------
+--Non Clustered Index
+--non cluster index on heap table is very bad
+--non cluster index on cluster inedex it's good
+
+--بررسی وجود جدول و خذف آن
+--IF OBJECT_ID('Customers2', 'U') is not null
+--	DROP Table Customers2
+-----
+--select * into Customers2 from Customers
+--SP_HELPINDEX Customers2
+
+--روی کلاستر ایندکس Non CLuster Index  ایجاد یک 
+--create Clustered index IX_customer2 ON Customers2(CustomerID)
+--create NonClustered Index IX_NonNcustomer2 On Customers2(country)
+--SP_HELPINDEX Customers2
+
+--select CustomerID,Country,City from Customers2
+--	where Country in('Sweden', 'Mexico')
+
+--select CustomerID,Country,City from Customers2 with(INDEX(IX_NonNcustomer2))
+--	where Country in('Sweden', 'Mexico')
+
+--استفاده از نان کلاستر اندیکس همپوشان
+--create nonCLustered Index IX_NC_Include On Customers2(CustomerID)
+--	include(City,Country)
+
+--create nonclustered columnstore index IX_rule ON Customers2(UID,EntityNo,CpNo)
+
